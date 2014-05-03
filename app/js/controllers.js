c161bf603fe192f3dc844a489b84c85532a2846a
Tapan Pandita <tapan.pandita@gmail.com>
2014-05-03 15:19:05 +0530

    visual updates, added audio player

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,10 +11,14 @@ podcastControllers.controller('PodcastListCtrl', ['$scope', '$http', function($s
     });
 }]);
 
-podcastControllers.controller('PodcastDetailCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-    var podcastId = $routeParams.podcastId
-    var url = 'http://127.0.0.1:8000/api/v1/podcast/' + podcastId + '/'
+podcastControllers.controller('PodcastDetailCtrl', ['$scope', '$http', '$routeParams', '$sce', function($scope, $http, $routeParams, $sce) {
+    var podcastId = $routeParams.podcastId;
+    var url = 'http://127.0.0.1:8000/api/v1/podcast/' + podcastId + '/';
     $http.get(url).success(function(data) {
         $scope.podcast = data;
+        for (var i=0; i < $scope.podcast.episodes.length; i++) {
+            var eurl = $scope.podcast.episodes[i].url;
+            $scope.podcast.episodes[i].url = $sce.trustAsResourceUrl(eurl);
+        }
     });
 }]);
